57e4edc83fdd639c512a18e970de41ecdddd770a
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-23 18:34:48 +0300

    square component

--- tic-tac/src/Components/Board.jsx
+++ tic-tac/src/Components/Board.jsx
@@ -1,27 +1,6 @@
 import React,{useState} from 'react'
 import './board.css'
-
-function Square() {
-
-  const [value , setValue] = useState(null);
-  const handleClick = () => {
-    setValue('X');
-  } 
-
-  return(
-    <>
-      <button 
-      className='square'
-      onClick={handleClick}
-      >
-        {value}
-      </button>
-    </>
-  )
-}
-
-
-
+import Square from './Square'
 
 const Board = () => {
   return (
---
+++ tic-tac/src/Components/Square.jsx
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+
+export default function Square() {
+
+    const [value , setValue] = useState(null);
+    const handleClick = () => {
+      setValue('X');
+    } 
+  
+    return(
+        <>
+        <button 
+            className='square'
+            onClick={handleClick}> 
+          {value}
+        </button>
+      </>
+    )
+  }
