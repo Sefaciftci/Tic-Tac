78943d49f6032a4366a793221c2afda22b0024d4
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-24 09:05:28 +0300

    create checkForWin funtion for winner

--- tic-tac/src/Components/Board.jsx
+++ tic-tac/src/Components/Board.jsx
@@ -12,7 +12,46 @@ const Board = () => {
       setTurn(turn => turn === 'X' ? 'O':'X');
 
       board[row][col] = turn;
-      console.log(board);
+
+      console.log(checkForWin());
+    }
+
+    const checkForWin = () => {
+
+      //satır kontrol
+        for(let i= 0 ; i<board.length;i++){
+          const row = board[i];
+          if(row[0] === row[1] && row[1]===row[2]&& row[0]){
+            return row[0]
+          }
+        }
+      //sutun kontrol
+        for(let i=0;i<board.length;i++){
+          const st0 = board[0][i]
+          const st1 = board[1][i]
+          const st2 = board[2][i]
+          
+          if(st0===st1 && st1===st2 && st0)
+              return st0;
+        }
+
+        //sagdan diagonal kontrol
+        const sagd0 = board[0][0];
+        const sagd1 = board[1][1];
+        const sagd2 = board[2][2];
+        if(sagd0===sagd1&&sagd1===sagd2&&sagd0){
+          return sagd0;
+        }
+        
+        //sagdan diagonal kontrol
+        const sold0 = board[0][2];
+        const sold1 = board[1][1];
+        const sold2 = board[0][2];
+        if(sold0===sold1&&sold1===sold2&&sold0){
+          return sold0;
+        }
+
+        return false
     }
 
 
